ebbd587477d04d69fa1f3dabf017b678375f6d94
stavr <grey-matter77@mail.ru>
2024-06-26 17:58:36 +0300

    Добавление докстрингов, файла README.md

--- main.py
+++ main.py
@@ -3,37 +3,44 @@ from src.class_load_HH import HeadHunterAPI
 from src.class_other import JSONSaver, Vacancy
 from src.utils import sort_salary, get_vacancies_by_salary
 
-# Преобразование набора данных из JSON в список объектов
-# vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
 
-
-
-# Сохранение информации о вакансиях в файл
-# json_saver = JSONSaver(vacancy)
-# json_saver.add_vacancy()
-# json_saver.delete_vacancy(vacancy)
-
-
-# Функция для взаимодействия с пользователем
 def user_interaction():
+    """
+    Функция для взаимодействия с пользователем
+    """
     search_query = input("Введите поисковый запрос: ")
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    # while True:
-    salary_range = input("Введите диапазон зарплат: ")  # Пример: 100000 - 150000
-        # if salary_range
+    while True:
+        salary_range = input("Введите диапазон зарплат (Пример: 100000 - 150000): ")
+        salary = []
+        for salar in salary_range:
+            if salar.isdigit():
+                salary.append(salar)
+        if len(salary) != 0:
+            break
 
     # Создание экземпляра класса для работы с API сайтов с вакансиями
     hh_api = HeadHunterAPI()
 
     # Получение вакансий с hh.ru в формате JSON
     hh_api.load_vacancies(search_query)
-    hh_vacancies_list = hh_api.vacancies
+
+    # Получение списка вакансий
+    hh_vacancies_list = hh_api.get_vacancies
+
+    # Сохранение загруженных вакансий в json файл
     json_saver = JSONSaver(VACANCIES_PATH)
     json_saver.add_vacancy(hh_vacancies_list)
     json_list = json_saver.loads_json()
+
+    # Создания списка объектов класса Vacancy
     vacancies = Vacancy.cast_to_object_list(json_list)
+
+    # СОртировака списка вакансий по зарплате
     ranged_vacancies = get_vacancies_by_salary(salary_range, vacancies)
     vacancies_sort = sort_salary(ranged_vacancies)
+
+    # Вывод первых N вакансий по зарплате
     top_vacancies = vacancies_sort[0:top_n]
     for vacancy in top_vacancies:
         print(vacancy)
@@ -41,17 +48,3 @@ def user_interaction():
 
 if __name__ == "__main__":
     user_interaction()
-
-
-
-
-
-
-    # ranged_vacancies = get_vacancies_by_salary(salary_range)
-    #
-    # sorted_vacancies = sort_vacancies(ranged_vacancies)
-    # top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    # print_vacancies(top_vacancies)
-
-
-
