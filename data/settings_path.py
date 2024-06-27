0f2bfd6410f475c00b5b77681b802614b641e076
stavr <grey-matter77@mail.ru>
2024-06-27 12:44:49 +0300

    Исправление Type хинта

--- data/settings_path.py
+++ data/settings_path.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 
 # создаем переменные с путями к нужным файлам для импорта:
@@ -6,8 +5,7 @@ PARENT_PATH = Path(__file__).parent.parent
 DATA_PATH = Path(PARENT_PATH, "data")
 VACANCIES_PATH = Path.joinpath(DATA_PATH, 'vacancies.json')
 
-print(VACANCIES_PATH)
-print(type(VACANCIES_PATH))
+
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,17 @@
-def sort_salary(list_vacancies):
+from src.class_other import Vacancy
+
+
+def sort_salary(list_vacancies: list[Vacancy]) -> list[Vacancy]:
+    """
+    Сортирует список экземпляров класса Vacancy по атрибуту salary
+    """
     return sorted(list_vacancies)
 
 
-def get_vacancies_by_salary(salary_range: str, list_vacancies: list) -> list:
+def get_vacancies_by_salary(salary_range: str, list_vacancies: list[Vacancy]) -> list[Vacancy]:
+    """
+    Возвращает список экземпляров класса Vacancy по заданным диапазонам зарплаты (атрибут salary)
+    """
     salary_range1, salary_range2 = [int(salary.strip()) for salary in salary_range.split('-')]
     list_vacancies_by_salary = []
     for vacancy in list_vacancies:
